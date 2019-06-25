0e5655f76552469673380f44e670b004234134db
Martin Christopher <christophersegoh@gmail.com>
2019-06-25 20:45:41 +0700

    sfgvav

--- detail.php
+++ detail.php
@@ -59,12 +59,14 @@
     <a href="index.php"><i class="fas fa-times-circle"></i></a>
     <img src="images/crm_<?php echo $id;?>.jpg" style="width:100%;">
     <div class="container">
-      <H2>DENIM</h2>
+      <H2>Basic Knitware</h2>
       <div>
-        <h5>Description</h5>
-          <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <small><b>Description</b></small>
+          <p class="description">Long sleeve knit sweater with a round neckline. Featuring side vents at the hem and ribbed trims.
+
+HEIGHT OF MODEL: 177 cm. / 5′ 9″</p>
       </div>
-      <div>
+      <div class="my-3">
         <h5>SIZE</h5>
         <button type="button" class="btn btn-outline-dark btn-lg" name="button">XS</button>
         <button type="button" class="btn btn-outline-dark btn-lg" name="button">S</button>
